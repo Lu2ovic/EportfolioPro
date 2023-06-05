d1fa735633b3f4843d31a6c27f79224743838198
Lu2ovic <ludovic.hauptmann1@gmail.com>
2023-06-05 14:46:33 +0200

    parallaxe + contact

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var paraEl = document.querySelectorAll("[data-speed]");
+		function parallax() {
+			paraEl.forEach(e => {
+				var rect = e.getBoundingClientRect();
+				var transY = e.dataset.offset || 0;
+				var posiOrigin = rect.top - transY + rect.height / 2;
+				var offset = (posiOrigin - window.innerHeight / 2) * e.dataset.speed;
+
+				if (rect.top < window.innerHeight ||
+					offset < window.innerHeight / 2) {
+					e.dataset.offset = offset;
+					e.style.transform = `translateY(${offset}px)`;
+				}
+			})
+		}
+		window.addEventListener("scroll", parallax, { passive: true });
+		parallax();
+
+
+
+function contact(info) 
+{ 
+    document.querySelector(".contact").classList.toggle("cacher")
+}
